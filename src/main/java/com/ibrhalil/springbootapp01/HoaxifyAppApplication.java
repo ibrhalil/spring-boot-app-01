96f668ec9f12c02b224b1d7560f5188338786fa1
Halil AYDIN <ibrhalilaydn@gmail.com>
2024-04-06 22:43:08 +0300

    create app file

---
+++ src/main/java/com/ibrhalil/springbootapp01/HoaxifyAppApplication.java
@@ -0,0 +1,13 @@
+package com.ibrhalil.springbootapp01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HoaxifyAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(HoaxifyAppApplication.class, args);
+	}
+
+}
